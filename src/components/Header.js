da6960d8f68b6f0520d33e00235fdf4b11de4142
Chris Ivey <chrisivey01@gmail.com>
2019-02-22 13:11:54 -0600

    added donation

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = props => {
             <div>
              <label id="title">WvW Kill Community Leaderboards</label>
             </div>
+
             <div>
                 Top weekly killer: {props.topWeeklyKiller !== '' ? props.topWeeklyKiller : null}
             </div>
@@ -17,6 +18,15 @@ const Header = props => {
                 <Link to="/leaderboard" onClick={props.leaderboardHandler}>
                     <button className="routeButtons">Top</button>
                 </Link>
+                <div style={{right:0}}>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_donations" />
+                    <input type="hidden" name="business" value="Y5DEZW3QBMNRW" />
+                    <input type="hidden" name="currency_code" value="USD" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+                </div>
             </div>
         </div>
     )
